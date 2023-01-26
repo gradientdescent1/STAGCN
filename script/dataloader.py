554e55cae91dab4d4e7953ad49d3e13ff8f66a30
Ipsit <mmkipsit@gmail.com>
2023-01-26 09:15:14 +0530

    Added custom covid dataset
    Added custom covid dataset. Made corresponding
    changes at all places where dataset is loaded
    
    Signed-off-by: Ipsit <mmkipsit@gmail.com>

--- script/dataloader.py
+++ script/dataloader.py
@@ -16,6 +16,8 @@ def load_adj(dataset_name):
         n_vertex = 325
     elif dataset_name == 'pemsd7-m':
         n_vertex = 228
+    elif dataset_name == 'covid':
+        n_vertex = 25
 
     return adj, n_vertex
 
